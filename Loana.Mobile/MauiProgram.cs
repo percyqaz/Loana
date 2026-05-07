514a67529961eb6a2b7c32aef96db6fb4fac1124
Percy Qaz <percyqaz@yahoo.co.uk>
2026-05-07 18:16:48 +0100

    chore: remove default 'projects'

--- Loana.Mobile/MauiProgram.cs
+++ Loana.Mobile/MauiProgram.cs
@@ -34,10 +34,8 @@ namespace Loana.Mobile
             builder.Services.AddSingleton<SeedDataService>();
             builder.Services.AddSingleton<ModalErrorHandler>();
             builder.Services.AddSingleton<MainPageModel>();
-            builder.Services.AddSingleton<ProjectListPageModel>();
             builder.Services.AddSingleton<ManageMetaPageModel>();
 
-            builder.Services.AddTransientWithShellRoute<ProjectDetailPage, ProjectDetailPageModel>("project");
             builder.Services.AddTransientWithShellRoute<TaskDetailPage, TaskDetailPageModel>("task");
             builder.Services.AddTransientWithShellRoute<ReviewPage, ReviewPageModel>("review");
             return builder.Build();
